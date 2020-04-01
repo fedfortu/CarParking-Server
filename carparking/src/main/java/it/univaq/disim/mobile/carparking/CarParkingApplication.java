5a5699e85e71a499658b32195d8255ff780d7d3f
denis1997 <denisdipatrizio4@gmail.com>
2020-04-01 18:53:29 +0200

    load project

---
+++ carparking/src/main/java/it/univaq/disim/mobile/carparking/CarParkingApplication.java
@@ -0,0 +1,15 @@
+package it.univaq.disim.mobile.carparking;
+
+import it.univaq.disim.mobile.carparking.business.impl.repositories.*;
+import it.univaq.disim.mobile.carparking.domain.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Date;
+
+public class CarParkingApplication {
+}
